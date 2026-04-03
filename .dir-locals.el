72605f78524dff09665f72f8984ab133a8956517
Samuel Loury <konubinixweb@gmail.com>
2026-04-03 23:59:20 +0200

    chore: Don't let my emacs conf pollute the tangled files

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,6 +2,7 @@
 ((org-mode
   . ((org-babel-default-header-args:python
       . ((:preserve-indentation . t)))
+     (org-id-link-to-org-use-id . nil)
      (eval . (load (expand-file-name "check-result.el"
                      (locate-dominating-file default-directory ".dir-locals.el"))
                    t)))))
