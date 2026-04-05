c030c39aaf15f314c160659b747e4bd657a878fc
Samuel Loury <konubinixweb@gmail.com>
2026-04-05 22:25:19 +0200

    refactor(smell): Avoid possible infinite loop

--- tests/helpers.sh
+++ tests/helpers.sh
@@ -3,6 +3,6 @@
 unset HISTFILE
 TMP=$(mktemp -d)
 trap 'rm -rf "$TMP"' EXIT
-export ROOT=$(while [ ! -f dagger.json ]; do cd ..; done && pwd)
+export ROOT=$(while [ ! -f dagger.json ]; do [ "$(pwd)" = / ] && { echo "dagger.json not found" >&2; exit 1; }; cd ..; done && pwd)
 export PATH="${ROOT}/tests:$PATH"
 # Test sandbox setup:1 ends here
