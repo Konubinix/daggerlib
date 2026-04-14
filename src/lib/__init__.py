2023971a6737b38153eb5b01f48b47832b9aa478
Samuel Loury <konubinixweb@gmail.com>
2026-04-14 16:40:29 +0200

    chore: Move the part dealing with main in a proper location

--- src/lib/__init__.py
+++ src/lib/__init__.py
@@ -1,5 +1,5 @@
-# [[file:../../TECHNICAL.org::*Module entry point][Module entry point:1]]
+# [[file:../main.org::+begin_src python :tangle lib/__init__.py][No heading:1]]
 """Reusable container helpers: base images, timezone, user setup, DinD, etc."""
 
 from .main import Lib as Lib
-# Module entry point:1 ends here
+# No heading:1 ends here
